14fcd793dd8c0ced050705e61c90f705bd0dcf0b
dingGator <kim@chucrew.net>
2015-07-30 10:27:32 -0400

    Bring picture of cat
    Bring in picture of cat

--- js/app_ko.js
+++ js/app_ko.js
@@ -1,11 +1,35 @@
-var ViewModel = function() {
+var Cat = function(){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	this.imgSrc = ko.observable('img/cat_picture1.jpg');
 	this.imgAttribution = ko.observable('http://www.flickr.com/photos/big')
+	this.nicknames =ko.observableArray(['Orange', 'Brown', 'Red', 'Blue', 'Green'])
+
+	this.title= ko.computed(function(){
+		var title;
+		var clicks = this.clickCount();
+		if (clicks < 5) {
+			title = 'Newborn';
+		} else if (clicks < 15) {
+			title = 'Infant';
+		} else if (clicks < 20) {
+			title = 'Child';
+		} else if (clicks < 25) {
+			title = 'Teen';
+		} else if (clicks < 30) {
+			title = 'Adult';
+		} else {
+			title = 'Ninja';
+		}
+		return title;
+
+	}, this);
+}
+var ViewModel = function() {
+	this.currentCat = ko.observable(new Cat());
 
 	this.incrementCounter = function(){
 		this.clickCount(this.clickCount() +1);
 	};
-}
+};
 ko.applyBindings(new ViewModel());
